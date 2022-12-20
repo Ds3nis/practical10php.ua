80f20e33ef0f36d36f023145d3871818256a7d4b
Ds3nis <dhuda566@gmail.com>
2022-12-20 15:58:15 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Головна</title>
+</head>
+<body>
+<div class="container">
+    <div class="container__title">
+        <h1>Головна</h1>
+    </div>
+    <a href="profile" target="_blank">Головна</a> <br>
+    <a href="aboutus" target="_blank">Про нас</a>
+</div>
+</body>
+</html>
